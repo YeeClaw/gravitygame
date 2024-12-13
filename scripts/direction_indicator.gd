8ab2319cc26f445c43092b7afed97b90c3d80614
Austin Colt <austin@jcolt.net>
2024-12-13 00:41:22 -0700

    TG-7 Fix direction_indicator script link

--- scripts/direction_indicator.gd
+++ scripts/direction_indicator.gd
@@ -7,7 +7,7 @@ func _ready():
 
 func _process(_delta):
     #Tells the direction indicator to look at the cursor
-    var flat_mouse_position: Vector2 = get_viewport().get_global_mouse_position()
+    var flat_mouse_position: Vector2 = get_viewport().get_mouse_position()
     var thiccc_mouse_position := Vector3(flat_mouse_position.x, 0, flat_mouse_position.y)
     print(thiccc_mouse_position)
     look_at(thiccc_mouse_position)
