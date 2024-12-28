e86e0a5814aa36661dc7c5ef1b8479c41fd64c00
Austin Colt <austin@jcolt.net>
2024-12-28 16:26:49 -0700

    Add some things?

---
+++ scripts/red_arrow.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+var mesh: MeshInstance3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mesh = $"Node/Layer_1"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
